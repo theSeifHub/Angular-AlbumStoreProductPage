e7d47a19b63dc4c33299becf7d03f295b9a057cc
Seif Eldeen Ahmed <seifahmed870@yahoo.com>
2020-09-29 13:37:24 +0200

    Completed module 5

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Http, Response } from "@angular/http";
 import { Album } from "./album";
+import { Product } from "./product";
 import { Observable } from "rxjs/Observable";
 
 import "rxjs/add/operator/map";
@@ -8,6 +9,7 @@ import "rxjs/add/operator/map";
 @Injectable()
 export class ProductService {
   private _albumUrl = "../assets/album.json";
+  private _productsUrl = "../assets/products.json";
 
   constructor(private _http: Http) {}
 
@@ -15,5 +17,11 @@ export class ProductService {
     return this._http
       .get(this._albumUrl)
       .map((response) => <Album>response.json());
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http
+      .get(this._productsUrl)
+      .map((response) => <Product[]>response.json());
   }
 }
